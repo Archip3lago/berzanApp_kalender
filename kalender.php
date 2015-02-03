1cf62785dd3e4cdf124a319efd6529b41b0c38d4
Archip3lago <truls.warvsten@tele2.se>
2015-02-03 14:03:06 +0100

    Databashämtning funkar
    Tar ner aktiviteter från databasen. Ska göra så att man får ett popup
    med info om aktivitet

--- kalender.php
+++ kalender.php
@@ -6,19 +6,18 @@
         <link rel="stylesheet" href="enklare.css">
     </head>
     <body>
-        
+
     </body>
 </html>
 <?php
-
 define("DB_SERVER", "localhost");
-        define("DB_USER", "root");
-        define("DB_PASSWORD", "");
-        define("DB_NAME", "berzanapp");
-        $dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8', DB_USER, DB_PASSWORD);
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "berzanapp");
+$dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8', DB_USER, DB_PASSWORD);
 
-        include 'data.php';
-        
+include 'data.php';
+
 echo "<div id='wrapper'>";
 
 $m = date('M');
@@ -26,35 +25,10 @@ $y = date('Y');
 
 skriv_ut_dagar($y, $m, $data[$y][$m]);
 
-<<<<<<< HEAD
-        
-//Skapa gömda dagar
-for($i = 1; $i <= $data[$y][$m]; $i++){
-    echo "<div class='stay_hidden dag'>";
-    echo "<p>Detta syns inte</p>";
-    echo "</div>";    
-}
 
-    $sql = "SELECT * FROM `aktiviteter` WHERE 1";
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute(); 
-    $aktiv = $stmt->fetchAll();
-
-for($i = 1; $i <= $this_length; $i++){
-    echo "<div class='dag'>";
-    echo "<p>" . $i . "</p>";
-    echo "<form method='GET'>";
-    echo "<input type='hidden' name='date' value='". $i ."'>";
-    echo "<input type='submit' name='laggTill_aktivitet' value='Ny aktivitet'>";
-    //här
-    
-    echo "</div>";
-}
-=======
->>>>>>> origin/master
 echo "</div>";
 
-if(isset($_GET["laggTill_aktivitet"])){
+if (isset($_GET["laggTill_aktivitet"])) {
     echo "<form method='GET'>";
     echo "<p>Titel</p>";
     echo "<input type='text' name='titel'>";
@@ -75,8 +49,8 @@ if(isset($_GET["laggTill_aktivitet"])){
     echo "</form>";
 }
 
-if(isset($_GET["action"]) and $_GET["action"] == "ny_aktivitet"){
-    
+if (isset($_GET["action"]) and $_GET["action"] == "ny_aktivitet") {
+
     $tmp_titel = filter_input(INPUT_GET, 'titel', FILTER_SANITIZE_SPECIAL_CHARS);
     $tmp_info = filter_input(INPUT_GET, 'info', FILTER_SANITIZE_SPECIAL_CHARS);
     $tmp_plats = filter_input(INPUT_GET, 'plats', FILTER_SANITIZE_SPECIAL_CHARS);
@@ -90,7 +64,7 @@ if(isset($_GET["action"]) and $_GET["action"] == "ny_aktivitet"){
     $tmp_minutslut = filter_input(INPUT_GET, 'minut_slut', FILTER_SANITIZE_SPECIAL_CHARS);
     $tid_slut = $tmp_timmeslut . $tmp_minutslut;
     $datum = $tmp_year . "-" . $tmp_month . "-" . $tmp_day . " " . $tmp_timme . ":" . $tmp_minut . ":00";
-    
+
     $sql = "INSERT INTO aktiviteter(titel, inlägg, plats, person, datum, tid) VALUES (:titel, :info, :plats, :person, :datum, :tid)";
     $stmt = $dbh->prepare($sql);
     $stmt->bindParam(":titel", $tmp_titel);
@@ -99,76 +73,67 @@ if(isset($_GET["action"]) and $_GET["action"] == "ny_aktivitet"){
     $stmt->bindParam(":tid", $tid_slut);
     $stmt->bindParam(":person", $tmp_anv);
     $stmt->bindParam(":plats", $tmp_plats);
-    $stmt->execute(); 
+    $stmt->execute();
 }
 
-function skriv_ut_dagar($year, $month, $displacement){
+function skriv_ut_dagar($year, $month, $displacement) {
     echo "<h1>" . $month . "</h1>";
-            echo "<div class='dagnamn'>";
-            echo "<h3>Måndag</h3>"; 
-            echo "</div>";
-            echo "<div class='dagnamn'>";
-            echo "<h3>Tisdag</h3>"; 
-            echo "</div>";
-            echo "<div class='dagnamn'>";
-            echo "<h3>Onsdag</h3>"; 
-            echo "</div>";
-            echo "<div class='dagnamn'>";
-            echo "<h3>Torsdag</h3>"; 
-            echo "</div>";
-            echo "<div class='dagnamn'>";
-            echo "<h3>Fredag</h3>"; 
-            echo "</div>";
-            echo "<div class='dagnamn'>";
-            echo "<h3>Lördag</h3>"; 
-            echo "</div>";
-            echo "<div class='dagnamn'>";
-            echo "<h3>Söndag</h3>"; 
-            echo "</div>";
-    
-    
-    for($i = 1; $i <= $displacement; $i++){
+    echo "<div class='dagnamn'>";
+    echo "<h3>Måndag</h3>";
+    echo "</div>";
+    echo "<div class='dagnamn'>";
+    echo "<h3>Tisdag</h3>";
+    echo "</div>";
+    echo "<div class='dagnamn'>";
+    echo "<h3>Onsdag</h3>";
+    echo "</div>";
+    echo "<div class='dagnamn'>";
+    echo "<h3>Torsdag</h3>";
+    echo "</div>";
+    echo "<div class='dagnamn'>";
+    echo "<h3>Fredag</h3>";
+    echo "</div>";
+    echo "<div class='dagnamn'>";
+    echo "<h3>Lördag</h3>";
+    echo "</div>";
+    echo "<div class='dagnamn'>";
+    echo "<h3>Söndag</h3>";
+    echo "</div>";
+
+
+    for ($i = 1; $i <= $displacement; $i++) {
         echo "<div class='stay_hidden dag'>";
-        echo "<p>Detta syns inte</p>"; 
+        echo "<p>Detta syns inte</p>";
         echo "</div>";
     }
-    
-    
-    
+
+
+
     $length = date('t');
-    for($i = 1; $i <= $length; $i++){
+    for ($i = 1; $i <= $length; $i++) {
+        $m = date('m');
         echo "<div class='dag'>";
         echo "<p>" . $i . "</p>";
+        letaAktivitet($year, $m, $i);
         echo "<form method='GET'>";
-        echo "<input type='hidden' name='date' value='dag_" . $i ."'>";
+        echo "<input type='hidden' name='date' value='dag_" . $i . "'>";
         echo "<input type='submit' value='Ny aktivitet'>";
         echo "</div>";
     }
+}
+
+function letaAktivitet($year, $month, $day) {
     
-}  
-
-
-
-
-
-////Skapa gömda dagar
-//for($i = 1; $i <= $data[$y][$m]; $i++){
-//    echo "<div class='stay_hidden dag'>";
-//    echo "<p>Detta syns inte</p>";
-//    echo "</div>";    
-//}
-//
-    $sql = "SELECT * FROM `aktiviteter` WHERE 1";
+    $dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8', DB_USER, DB_PASSWORD);
+    if (strlen($day) == 1) {
+        $day = 0 . $day;
+    }
+    $datum = $year . "-" . $month . "-" . $day;
+    $sql = "SELECT * FROM aktiviteter WHERE datum LIKE '" . $datum . "%'";
     $stmt = $dbh->prepare($sql);
-    $stmt->execute(); 
-    $aktiv = $stmt->fetchAll();
-//
-//for($i = 1; $i <= $this_length; $i++){
-//    echo "<div class='dag'>";
-//    echo "<p>" . $i . "</p>";
-//   
-//    echo "<form method='GET'>";
-//    echo "<input type='hidden' name='date' value='". $i ."'>";
-//    echo "<input type='submit' name='laggTill_aktivitet' value='Ny aktivitet'>";
-//    echo "</div>";
-//}
+    $stmt->execute();
+    $aktiviteter = $stmt->fetchAll();
+    foreach ($aktiviteter as $aktivitet) {
+        echo "<a href=''>" . $aktivitet["titel"] . "</a>";
+    }
+}
